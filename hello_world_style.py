ec7361d52a733457fa946fd2deb34acef47acf8f
Aaron Rivelino Vargas Kononenko <aaron.rivelino.vargas-kononenko@stu.ocsb.ca>
2025-02-13 03:00:49 +0000

    hello_world_style.py

---
+++ hello_world_style.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# Created By: Aaron
+# Date: 2025-02-12
+# The code is a Hello world program, displaying sentences
+
+
+def main():
+    print("Hello, World")
+    print("Hello, Aaron")
+
+
+if __name__ == "__main__":
+    main()
